5d2a13dfe1bdf5794239a6fac16fb45e2e45c464
evanfang0054 <380616679@qq.com>
2026-06-25 11:37:59 +0800

    feat(web): 新增 EmptyState 公共组件

--- demo/fruit-shop/packages/web/src/components/ui/index.ts
+++ demo/fruit-shop/packages/web/src/components/ui/index.ts
@@ -2,3 +2,4 @@ export { Button } from './Button';
 export { IconButton } from './IconButton';
 export { NavBar } from './NavBar';
 export { BottomActionBar } from './BottomActionBar';
+export { EmptyState } from './EmptyState';
